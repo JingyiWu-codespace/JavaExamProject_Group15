9fbea605e2f0ecac934c5a6b67cb7678ea2c2db1
Hossein <hossenmsignin@gmail.com>
2023-12-06 01:29:49 +0330

    inventory class includes all the needed functionality

--- Player.java
+++ Player.java
@@ -1,5 +1,6 @@
 package WPO_final;
 
+import WPO_final.Entity.Items;
 import WPO_final.Entity.Rooms;
 
 import java.util.ArrayList;
@@ -7,28 +8,22 @@ import java.util.List;
 
 public class Player {
     private Rooms currentRoom;
-    private List<String> inventory;
+    private final Inventory inventory;
     public Player() {
         // 默认玩家开始在 "lobby"
         currentRoom = Rooms.ROOM_LOBBY; //default start in Lobby
-        inventory = new ArrayList<>();
-    }
-
-    public String getInventory() {
-        return inventory.isEmpty() ? "nothing" : String.join(", ", inventory);
+        inventory = new Inventory(new Items[]{}); //default empty inventory
     }
 
     public Rooms getCurrentRoom(){
         return this.currentRoom;
     };
-    public void addItemToInventory(String item){
-        inventory.add(item);
-    }
-    public boolean removeItemFromInventory(String item) {
-        // The remove() method itself returns true if the list contained the specified element
-        return inventory.remove(item);
-    }
+
     public void setCurrentRoom(Rooms newRoom){
         this.currentRoom = newRoom;
     };
+
+    public Inventory getInventory() {
+        return this.inventory;
+    }
 }
