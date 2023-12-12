1157d5b9d4604269a44c8acdc79420d0badef844
Hossein <hossenmsignin@gmail.com>
2023-12-12 14:42:35 +0330

    moved everything into main

--- GameManager.java
+++ GameManager.java
@@ -3,33 +3,31 @@ package JavaExamProject_Group15;
 import java.io.IOException;
 
 public class GameManager {
-    private final UserParser userIo = new UserParser();
-    private final StoryTeller storyTeller = new StoryTeller();
-
-    private void gameLoop() {
+    public static void main(String[] args) throws IOException {
+        final UserParser userIo = new UserParser();
+        final StoryTeller storyTeller = new StoryTeller();
         int timeCounter = -1;
+
         while (true) {
             timeCounter++;
 
             // check victory
-            if (this.storyTeller.check_victory()) {
+            if (storyTeller.check_victory()) {
                 System.out.println("A good days work. Now go sleep.");
                 break;
             }
 
             // check timeout
-            if (timeCounter > storyTeller.gameTimeout)
+            if (timeCounter > storyTeller.gameTimeout) {
                 System.out.println("You are late and people died. You failed.");
+                break;
+            }
 
             // get and run action
-            UserParser.ParsedData parsed_codes = this.userIo.getUserInput();
+            UserParser.ParsedData parsed_codes = userIo.getUserInput();
             if (parsed_codes.itemCode == null)
-                this.storyTeller.executeAction(parsed_codes.actionCode, parsed_codes.roomCode);
-            else
-                this.storyTeller.executeAction(parsed_codes.actionCode, parsed_codes.itemCode);
+                storyTeller.executeAction(parsed_codes.actionCode, parsed_codes.roomCode);
+            else storyTeller.executeAction(parsed_codes.actionCode, parsed_codes.itemCode);
         }
-    }
-
-    public static void main(String[] args) throws IOException {
     }
 }
