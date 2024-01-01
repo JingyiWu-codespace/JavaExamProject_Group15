35d2f6ca2533020010b60bff47b8084489f4d8dd
Hossein <hossenmsignin@gmail.com>
2024-01-01 03:46:20 +0100

    new rooms

--- Entity/Rooms.java
+++ Entity/Rooms.java
@@ -3,60 +3,82 @@ package JavaExamProject_Group15.Entity;
 import JavaExamProject_Group15.Inventory;
 
 public enum Rooms {
-    ROOM_HALLWAY("hallway", "First floor, the elevators are here"),
-    ROOM_2nd_HALLWAY("2nd hallway", "Second floor, access to patient rooms"),
-    ROOM_402("402", "Room 402, where patient Alice is staying"),
-    ROOM_LOBBY("lobby", "The main lobby of the hospital, bustling with activity");
+    // Floor 0
+    ROOM_ER("Emergency Room", "the place where you don't wanna be in", new String[]{"emergency", "er"}, new Items[]{Items.ER_PATIENT, Items.ITEM_ER_STORAGE_KEY,Items.ER_PATIENT2,Items.ER_PATIENT3}),
+    ROOM_ER_STORAGE("ER Storage place", "the ER storage room", new String[]{"storage"}, new Items[]{Items.BANDAGE,Items.WHEEL_CHAIR}),
+    ROOM_RECEPTION_DESK("ReceptionDesk"," serving as the first point of contact for patients, visitors, and anyone entering the hospital.",new String[]{"info","rd"},new Items[]{Items.REGISTER_FORM}),
+    ROOM_PHARMACY("Pharmacy","dispensing prescribed medications to patients.",new String[]{"medicine room","pharm"},new Items[]{Items.MEDICINE}),
+    ROOM_LABORATORY("laboratory","Perform tests like blood analysis to obtain vital health information about patients.",new String[]{"lab","test room"},new Items[]{Items.TEST_RESULT}),
+    // Floor 1
+    ROOM_RADIOLOGY("x-ray","Conduct imaging tests, acquire diagnostic information, for example X-ray analysis.",new String[]{"xray","X-ray"},new Items[]{Items.XRAY_IMAGE}),
+    ROOM_Ultrasonic("Ultrasonic","ultrasound procedures include abdominal ultrasounds, echocardiograms, and obstetric ultrasounds.",new String[]{"Ultra"},new Items[]{Items.ULTRA_REPORT}),
+    ROOM_ICU("ICU", "An intensive care unit (ICU) room is equipped with advanced monitoring and life support equipment for critically ill patients.", new String[]{"icu"}, new Items[]{}),
+    ROOM_SURGERY("Surgery Room", "sterile environment where surgical procedures are performed by medical professionals.", new String[]{"surgery"}, new Items[]{Items.BISTOURY}),
+    ROOM_WARD
+            ("Patient Ward", "Ward rooms provide comfortable accommodations for patients during their hospital stay.", new String[]{"ward"}, new Items[]{Items.ER_PATIENT}),
+    // Floor 2
+    ROOM_OBSTETRICS("Obstetrics Medicial Area", "designed for childbirth and maternity care, ensuring a safe and comfortable environment for expectant mothers.", new String[]{"baby room", "enfant"}, new Items[]{Items.CRADLE, Items.INCUBATOR}),
+    ROOM_GYNECOLOGY("Gynecology Medicial Area", "dedicated to women's health and provide services related to gynecological examinations and treatments.", new String[]{}, new Items[]{}),
+    ROOM_REHABILITATION("Rehabilitation Room", "are equipped for physical and occupational therapy to aid patients in their recovery and mobility.", new String[]{}, new Items[]{Items.WHEEL_CHAIR}),
+    ROOM_ENT("ENT Medical Area", "are specialized for examinations and treatments related to ear, nose, and throat conditions.", new String[]{"ent", "eye","nose","throat"}, new Items[]{Items.EYE_CHART}),
+    ROOM_OFFICE("Doctor Office", "You should communicate with doctor then get prescription instead of take the prescription directly .", new String[]{"office","doctionoffice","df"}, new Items[]{Items.DOCTOR,Items.COMPUTER, Items.PRESCRIPTION});
 
-    private final Inventory inventory;
-    private Rooms[] exits; // Adjacent rooms
-    private RoomInfo roomInfo;
-
-    // ************************************************************
-
-    private final RoomInfo room_info;
-
-    Rooms(String name, String description) {
-        this.room_info = new RoomInfo(name, description, new String[]{});
-        this.inventory = new Inventory(new Items[]{});
+    static {
+        ROOM_ER_STORAGE.setExits(ROOM_ER);
+        ROOM_ER.setExits(ROOM_RECEPTION_DESK);
+        ROOM_RECEPTION_DESK.setExits(ROOM_ER, ROOM_PHARMACY);
     }
+
     public void setExits(Rooms... exits) {
-        this.exits = exits;
+        this.entityData.exits = exits;
     }
     public Rooms[] getExits() {
-        return exits;
-    }
-    static {
-        ROOM_HALLWAY.setExits(ROOM_2nd_HALLWAY);
-        ROOM_2nd_HALLWAY.setExits(ROOM_HALLWAY, ROOM_402);
-        ROOM_402.setExits(ROOM_2nd_HALLWAY);
-        ROOM_LOBBY.setExits(ROOM_HALLWAY);
-    }
-
-
-    public String getName() {
-        return this.room_info.getName();
-    }
-
-    public String[] get_command_array() {
-        return this.room_info.get_command_array();
-    }
-
-    public String get_description() {
-        return this.room_info.getDescription();
-    }
-
-    public Boolean check_command(String alias) {
-        return this.room_info.checkCommand(alias);
+        return this.entityData.exits;
     }
 
     public Inventory getInventory() {
-        return this.inventory;
+        return this.entityData.inventory;
     }
 
-    private class RoomInfo extends BaseEntity {
-        RoomInfo(String name, String description, String[] aliases) {
+    // ************************************************************
+    private final Rooms.RoomDataHolder entityData;
+
+    public String getName() {
+        return this.entityData.getName();
+    }
+
+    public String getDescription() {
+        return this.entityData.getDescription();
+    }
+
+    public Boolean checkCommand(String alias) {
+        return this.entityData.checkCommand(alias);
+    }
+
+    public String[] getAliases() {
+        return this.entityData.getAliases();
+    }
+
+    Rooms(String name, String description, String[] aliases, Items[] item_list) {
+        this.entityData = new RoomDataHolder(name, description, aliases, item_list);
+    }
+
+    static class RoomDataHolder extends BaseEntityDataHolder {
+        private Rooms[] exits;
+        private final Inventory inventory;
+
+        RoomDataHolder(String name, String description, String[] aliases, Items[] item_list) {
             super(name, description, aliases);
+            this.exits = new Rooms[]{};
+            this.inventory = new Inventory(item_list);
+        }
+
+        public Rooms[] getExits() {
+            return this.exits;
+        }
+
+        public void setExits(Rooms... exits) {
+            this.exits = exits;
         }
     }
 }
